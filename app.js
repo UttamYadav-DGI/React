f38316cf05a6b29a92343dc83667494da07775c2
UttamYadav-DGI <uttamyadav9560@gmail.com>
2025-07-01 14:47:41 +0530

    chore: initialize project with package.json and basic dependencies,learn about parcel ,parcel small components,features etc

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1",{id:"subchild"},"radheshyam"),React.createElement("h2",{},"i am h2"),React.createElement("h1",{id:"subchild"},"radheshyam")]
-    )
-)
-
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 const papa=React.createElement('div',{id:"papa"},React.createElement('div',{id:"bacha"},React.createElement('h1',{},"bacha hello world")));
-
+     
 // root.render(papa);
 const heading=React.createElement("h1",{},"hello world by react");
 const root=ReactDOM.createRoot(document.getElementById("root"));
